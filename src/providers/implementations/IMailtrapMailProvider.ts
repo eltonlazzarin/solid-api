fd861f808f27c65714216948a5ba44e7bd74e7a6
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-08-03 22:26:01 -0300

    mailtrap provider

---
+++ src/providers/implementations/IMailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "./../IMailProvider";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "37400e7befa6b1",
+        pass: "812837e2207a53",
+      },
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
